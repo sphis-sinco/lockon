f561ad47b3b4d5858929155baa9beb99e486fd72
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-24 21:05:14 -0500

    LOT02-TARG

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,16 @@
 package;
 
+import cartManagement.CartChecker;
+
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
+		#if sys
+		if (CartChecker.VALID_CARTS_SERIALS.contains('LOT02-TARG'))
+			trace('LOT02-TARG');
+		#end
+
 		super.create();
 	}
 
